c556f0b08a37723391743cd1ea52a9b798305a5a
Marcia Gameleira <gameleira.mag@hotmail.com>
2026-08-04 09:01:50 -0300

    t1c1: cria projeto indexa.

---
+++ t1c1-web-components/indexa/src/app/app.ts
@@ -0,0 +1,13 @@
+import { Component, signal } from '@angular/core';
+import { RouterOutlet } from '@angular/router';
+import { Container } from "./components/container/container";
+
+@Component({
+  selector: 'app-root',
+  imports: [RouterOutlet, Container],
+  templateUrl: './app.html',
+  styleUrl: './app.css'
+})
+export class App {
+  protected readonly title = signal('indexa');
+}
